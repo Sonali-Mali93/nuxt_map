8445e52645bd76b4008e9aec73d6740b2edacc83
Sonali Mali <sonali.mali@olacabs.com>
2022-08-05 18:10:52 +0530

    integrated mapbox in nuxt project

---
+++ pages/map.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>Test</div>
+    <div id=”map”></div>
+</template>
+<script>
+import mapboxgl from 'mapbox-gl'
+export default {
+
+    access_token: "pk.eyJ1Ijoic29jaWFsZXhwbG9yZXIiLCJhIjoiREFQbXBISSJ9.dwFTwfSaWsHvktHrRtpydQ",
+    map() {
+
+        
+    }
+}
+</script>
+<style>
+#map {
+    width: 100%;
+    height: 100vh;
+}
+</style>
